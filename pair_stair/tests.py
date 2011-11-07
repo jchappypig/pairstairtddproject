05be185c4feea16d5e439bc9b361931024f68fda
Huanhuan Huang <jchappypig@hotmail.com>
2011-11-08 01:12:16 +0800

    fix unit test failure on go to add programmer page

--- pair_stair/tests.py
+++ pair_stair/tests.py
@@ -1,16 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django import test
+from django.test.client import Client
 
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestAddProgrammer(test.TestCase):
+    def test_got_to_add_programmer_page(self):
+        response = Client().get("/addProgrammer/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_programmer.html')
--- pair_stair/tests/add_programmer_test.py
+++
@@ -1,7 +0,0 @@
-from django import test
-from django.test.client import Client
-
-class TestAddProgrammer(test.TestCase):
-    def test_got_to_add_programmer_page(self):
-        response = Client().get("/addProgrammer")
-        self.assertEqual(response.status_code, 200)
